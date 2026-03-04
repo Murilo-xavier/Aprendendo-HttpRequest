2238eb9a8f5cb7fc77a4621d5c44742714978125
Murilo Xavier <muriloxmacedo@gmail.com>
2026-03-04 20:59:51 -0300

    utilizando a biblioteca File do java.io, foi adicionado a funcionalidade de escrever o conteudo dos objetos Titulo em um arquivo .json

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -1,10 +1,13 @@
 package br.com.alura.screenmatch.principal;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import com.google.gson.FieldNamingPolicy;
@@ -18,34 +21,50 @@ import br.com.alura.screenmatch.modelos.TituloOmdb;
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite um filme para busca: ");
-        var busca = sc.nextLine();
-        
-        String apiKey = "4d4d70c9";
-        String endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&apikey=" + apiKey;
+        String busca = "";
+        List<Titulo> listaTitulos = new ArrayList<>();
+        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).setPrettyPrinting().create();
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(endereco))
-                .build();
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
-        String json = response.body();
-        System.out.println(json);
+        while (!busca.equalsIgnoreCase("sair")) {
 
-        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
-        //Titulo meuTitulo = gson.fromJson(json, Titulo.class);
-        TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
-        System.out.println("\n-- MEU TITULO OMDB: "+ meuTituloOmdb);
+            System.out.println("Digite um filme para busca: ");
+            busca = sc.nextLine();
 
-        try{
-            Titulo meuTitulo = new Titulo(meuTituloOmdb);
-            System.out.println("\n-- MEU TITULO: "+ meuTitulo);
-        }catch (ErroDeConversaoDeAnoException e){
-            System.out.println(e.getMessage());
-        }catch (IllegalArgumentException e){
-            System.out.println("Erro no argumento da busca: " + e.getMessage());
+            if (busca.equalsIgnoreCase("sair")) {
+                break;
+            }
+
+            String apiKey = "4d4d70c9";
+            String endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&apikey=" + apiKey;
+
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(endereco))
+                    .build();
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            String json = response.body();
+            System.out.println(json);
+
+            // Titulo meuTitulo = gson.fromJson(json, Titulo.class);
+            TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+            System.out.println("\n-- MEU TITULO OMDB: " + meuTituloOmdb);
+
+            try {
+                Titulo meuTitulo = new Titulo(meuTituloOmdb);
+                System.out.println("\n-- MEU TITULO: " + meuTitulo);
+                listaTitulos.add(meuTitulo);
+
+            } catch (ErroDeConversaoDeAnoException e) {
+                System.out.println(e.getMessage());
+            } catch (IllegalArgumentException e) {
+                System.out.println("Erro no argumento da busca: " + e.getMessage());
+            }
         }
+
+        FileWriter escrita = new FileWriter("filmes.json");
+        escrita.write(gson.toJson(listaTitulos));
+        escrita.close();
         System.out.println("--- FIM ---");
         sc.close();
     }
