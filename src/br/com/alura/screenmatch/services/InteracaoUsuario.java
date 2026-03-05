a887ae700c07035249b8dcb2ad1cea9d2e61f85d
Murilo Xavier <muriloxmacedo@gmail.com>
2026-03-04 21:47:11 -0300

    feat: add InteracaoUsuario class for user interaction and movie search input

---
+++ src/br/com/alura/screenmatch/services/InteracaoUsuario.java
@@ -0,0 +1,18 @@
+package br.com.alura.screenmatch.services;
+
+import java.util.Scanner;
+
+public class InteracaoUsuario {
+    private Scanner sc = new Scanner(System.in);
+    private String busca = "";
+
+    public String solicitarBusca() {
+        System.out.println("Digite um filme para busca: ");
+        busca = sc.nextLine();
+        return busca;
+    }
+    
+    public String getBusca() {
+        return busca;
+    }
+}
